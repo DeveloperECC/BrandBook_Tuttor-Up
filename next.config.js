44868c0f5239032649080f01919a643c7665aa24
DeveloperECC <elmercabrerac5@gmail.com>
2026-03-04 04:19:17 +0000

    Vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 'export' genera la carpeta 'out' con HTML estático
-  output: 'export',
-  // Importante: Si tu repo se llama 'BrandBook_Tuttor-Up', ponlo aquí.
-  // Si estás en local (npm run dev), esto no afecta.
-  basePath: process.env.NODE_ENV === 'production' ? '/BrandBook_Tuttor-Up' : '',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
